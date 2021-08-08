9f8106d299fd9988f3303b76f675185c01c2551e
michelle1574 <70703680+michelle1574@users.noreply.github.com>
2021-08-09 00:52:34 +0200

    Added automatic download settings
    (error when launching without parameters, not fixed)
    Please edit script and send to michalkichal1@outlook.com

--- down_uup.cmd
+++ down_uup.cmd
@@ -1,13 +1,32 @@
 @echo off
+if %1==auto ( goto auto ) else ( goto next )
+:next 
+if %1==noauto ( goto bgn ) else ( goto next2 )
+:next2
+if %1==help ( goto help ) else ( goto bgn )
+goto menu
+:auto
+set uupfold=%2
+if %3==yes ( set del-aria2=1 ) else if %3==no ( set del-aria2=0 )
+set id=%4
+set edt=%5
+set lng=%6
+if %7==uupdump ( set errorlevel=1 ) else if %7==uupgen ( set errorlevel=2 )
+if %8==yes ( set run_convert=1 ) else if %8==no ( set run_convert=0 )
+goto start_auto
+
+:bgn
 set run_convert=0
 set del-aria2=1
 :menu
 cls
 if %run_convert% neq 0 ( echo 1. Run convertion process: Enabled ) else if %run_convert% equ 0 ( echo 1. Run convertion process: Disabled )
 if %del-aria2% neq 0 ( echo 2. Delete generated script: Enabled ) else if %run_convert% equ 0 ( echo 2. Delete generated script: Disabled )
+echo 3. Help
 set /p "enbl=Select option to enable or disable and press Enter. Type download to begin: "
 if %enbl%==1 goto check1
 if %enbl%==2 goto check2
+if %enbl%==3 goto help
 if %enbl%==download goto work
 
 :check1
@@ -33,8 +52,12 @@ goto getfiles
 echo 1. UUPDump
 echo 2. UUP generation project
 choice /c 12 /m "Select server"
+goto start_auto
+:start_auto
 if %errorlevel% equ 2 (aria2c -o"aria2_script.txt" "https://uup.rg-adguard.net/api/GetFiles?id=%id%&lang=%lng%&edition=%edt%&txt=yes") else if %errorlevel% equ 1 (aria2c -o"aria2_script.txt" "https://uupdump.net/get.php?id=%id%&pack=%lng%&edition=%edt%&aria2=2")
-if not exist %uupfold% ( mkdir %uupfold%&goto work2 ) else ( goto work2 )
+if not exist %uupfold% ( mkdir %uupfold%&goto chck ) else ( goto chck )
+goto chck
+:chck
 find /i "error" aria2_script.txt >nul
 if %errorlevel% equ 0 goto LinkErrGen
 if %errorlevel% equ 1 goto work2
@@ -67,3 +90,15 @@ echo Generating links has failed.
 echo Try again later.
 choice /c 01 /m "Press 0 to exit, 1 to try again"
 if %errorlevel%==0 ( exit ) else if %errorlevel%==1 ( goto getfiles )
+
+:help
+echo To use these options first add argument auto or if you don't want to use them type noauto
+echo First is download folder
+echo Second is Download script deletion (yes or no)
+echo Third is ID of download
+echo Fourth is edition
+echo Fifth is language
+echo Sixth is server of generating
+echo Seventh if convertion process to run (yes or no)
+pause
+goto menu
